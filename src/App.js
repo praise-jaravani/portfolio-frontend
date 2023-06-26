4f059c64515f417174d1e640613b636568665911
praise-jaravani <jaravanipraise@gmail.com>
2023-06-26 14:19:36 +0000

    Updated Site with Functional Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/projects' exact element={<Projects/>} />
+        <Route path='/about_me' exact element={<AboutMe/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
